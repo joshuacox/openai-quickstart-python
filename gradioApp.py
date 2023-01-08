4feb6043b44bb634b34a6c6b4bc14e700a623b69
Josh Cox <josh@webhosting.coop>
2023-01-07 19:22:34 -0600

    gradioApp.py

--- gradioApp.py
+++ gradioApp.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import sys
 import tempfile
 import torch
+from PIL import Image
 from diffusers import StableDiffusionPipeline
 
 import gradio as gr
@@ -19,9 +20,17 @@ def image_classifier(inp):
 
 def draw(inp):
     drawing = inp
-    pipe = StableDiffusionPipeline.from_pretrained("../stable-diffusion-v1-5", torch_dtype=torch.float16)
+    drawing_filename = 'images/' + drawing.replace(' ', '_') + '.png'
+    if os.path.exists(drawing_filename):
+        print("found drawing ", drawing_filename)
+        return Image.open(drawing_filename) 
+    print("generating drawing '", drawing, "'", drawing_filename)
+    #pipe = StableDiffusionPipeline.from_pretrained("../stable-diffusion-v1-5", torch_dtype=torch.float16)
+    pipe = StableDiffusionPipeline.from_pretrained("../stable-diffusion-2", torch_dtype=torch.float16)
     pipe = pipe.to("cuda")
     image = pipe(drawing).images[0]  
+    image.seek(0)
+    image.save(drawing_filename)
     return image
 
 demo = gr.Interface(fn=draw, inputs="text", outputs="image")
